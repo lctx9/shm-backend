d04de1d04767a2db3416976ba521db1786da81f6
T1npu <ntp1112k5@gmail.com>
2026-07-20 16:11:05 +0700

    fix: permitAll exact path /ws-chat to allow websocket connection handshake

--- src/main/java/com/backend/config/SecurityConfig.java
+++ src/main/java/com/backend/config/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
+                                "/ws-chat",
                                 "/ws-chat/**"
                         ).permitAll()
                         .anyRequest().authenticated()
